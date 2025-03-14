869e0e7cbf53bab778a0c311879c04fd3cf88ce9
Roman Bogoudinov <r.bogoudinov@mac.com>
2025-03-14 12:21:06 +0200

    NodeBasedQueue class .enqueue() .dequeue() methods

--- data_structs_node_based_queue.py
+++ data_structs_node_based_queue.py
@@ -1,3 +1,4 @@
+import data_structs_doubly_linked_list_remove_insert_method
 class ListNode:
     def __init__(self, data=None, next=None, prev=None):
         self.data = data
@@ -6,17 +7,31 @@ class ListNode:
 
 class Queue():
     def __init__(self):
-        self._InboundStack = ListNode()
-        self._OutboundStack = ListNode()
+        self._first = None
+        self._last = None
         self._size = 0
 
     def __repr__(self):
         return
 
     def enqueue(self, data):
-
-        return
+        node_to_add = ListNode(data, next = self._first, prev= None)
+        if not self._first:
+            self._first = self._last = node_to_add
+        else:
+            self._last.prev = node_to_add
+            self._last = node_to_add
+        self._size +=1
 
     def dequeue(self):
 
+
+        self._size -= 1
         return
+
+
+def main():
+    queue = Queue()
+    queue.enqueue('A')
+    print(queue)
+main()
