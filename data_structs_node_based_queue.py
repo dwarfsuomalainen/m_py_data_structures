c18b13cd4ef5834aff364f139b385ee20420dd2e
Roman Bogoudinov <r.bogoudinov@mac.com>
2025-03-14 14:04:45 +0200

    Node based class Queue() methods .enqueue() (LIFO & FIFO) .dequeue() completed

--- data_structs_node_based_queue.py
+++ data_structs_node_based_queue.py
@@ -10,28 +10,85 @@ class Queue():
         self._first = None
         self._last = None
         self._size = 0
-
+    #LIFO repr
+    '''def __repr__(self):
+        current_node = self._last
+        values = ''
+        while current_node:
+            values += f', {current_node.data}'
+            current_node = current_node.next
+        plural = '' if self._size == 1 else 's'
+        return f'<Queue ({self._size} element{plural}): [{values.lstrip(", ")}]>'''''
+    # FIFO method
     def __repr__(self):
-        return
-
+        current_node = self._first
+        values = ''
+        while current_node:
+            values += f', {current_node.data}'
+            current_node = current_node.next
+        plural = '' if self._size == 1 else 's'
+        return f'<Queue ({self._size} element{plural}): [{values.lstrip(", ")}]>'
+    # LIFO method
+    '''def enqueue(self, data):
+        node_to_add = ListNode(data, next = self._first, prev= None)
+        if not self._first:
+            self._first = self._last = node_to_add
+        else:
+            current_node = self._last
+            current_node.prev = node_to_add
+            node_to_add.next = current_node
+            self._last = node_to_add
+        self._size +=1'''
+    # FIFo method
     def enqueue(self, data):
         node_to_add = ListNode(data, next = self._first, prev= None)
         if not self._first:
             self._first = self._last = node_to_add
         else:
-            self._last.prev = node_to_add
-            self._last = node_to_add
+            self._first.prev = node_to_add
+            self._first = node_to_add
         self._size +=1
 
     def dequeue(self):
+        if not self._size: return None
 
+        node_to_remove = self._last
+        prev_node = node_to_remove.prev
 
+        if self._size == 1:
+            self._first = self._last = None
+        else:
+            prev_node.next = None
+            self._last = prev_node
+        data = node_to_remove.data
+        del node_to_remove
         self._size -= 1
-        return
+        print(data)
+        return data
 
 
-def main():
+'''def main():
+    queue = Queue()
+    queue.enqueue('A')
+    queue.enqueue('A')
+    queue.enqueue('B')
+    queue.enqueue('C')
+    print(queue)
+main()'''
+
+'''def main ():
     queue = Queue()
     queue.enqueue('A')
     print(queue)
-main()
+    queue.enqueue('B')
+    print(queue)
+    queue.enqueue('C')
+    print(queue)
+    queue.enqueue('D')
+    print(queue)
+    queue.enqueue('D')
+    print(queue)
+    val = queue.dequeue()
+    val = queue.dequeue()
+    print(val, queue)
+main()'''
