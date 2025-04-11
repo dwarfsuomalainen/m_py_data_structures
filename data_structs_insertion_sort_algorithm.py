3b3a302af4f1bb4284f5113dc732c37cb8df1255
Roman Bogoudinov <r.bogoudinov@mac.com>
2025-04-11 13:01:26 +0300

    insertion sort

---
+++ data_structs_insertion_sort_algorithm.py
@@ -0,0 +1,22 @@
+def insertion_sort(array):
+    """
+    Sort the array using the Insertion sort algorithm
+
+    Parameters:
+    - array: The array to be sorted
+
+    Returns: Nothing. The array is sorted in-place.
+    """
+    if array[0] > array[1]:
+        array[0], array[1] = array[1], array[0]
+    print(array)
+    while True:
+        for i in range(1, len(array) + 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+
+def main():
+    array = [6, 8, 5, 1, 2]
+    insertion_sort(array)
+    print(array)
+main()
